f15596a1ceabbad3700737952e99d0e6e9150d00
Osura Yahampath <osuradesilva3@gmail.com>
2025-01-10 21:07:47 +0530

    created user delete function

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,3 +59,11 @@ export function loginUser(req,res){
         }   
     ) 
 }
+
+export function deleteUser(req,res){
+    User.deleteOne({email : req.body.email}).then(()=>{
+        res.json({
+            message : "student deleted"
+        })
+    })
+};
