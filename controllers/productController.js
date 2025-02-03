d59de7e6ca5e01d190613082955b56cfb1580c74
Osura Yahampath <osuradesilva3@gmail.com>
2025-02-03 14:01:14 +0530

    create function call createProduct in ProductController file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,26 @@
 import Product from "../models/products.js";
+import { isAdmin } from "./userController.js";
 
 export function createProduct(req,res){
-    
+
+    if(!isAdmin(req)){
+        res.json({
+            message : "please login as an administrator to add product"
+        })
+        return
+    }
+
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>(
+        res.json({
+            message : "product created"
+        })
+    )).catch((error)=>{
+        res.json({
+            message : error
+        })
+    })
 }
